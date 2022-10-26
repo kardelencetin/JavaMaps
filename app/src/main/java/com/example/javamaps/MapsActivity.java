e04e88ccdc8e49133cb92c84b1579c65f4bcaa91
kardelencetin <krdlnctn98@gmail.com>
2022-10-27 00:37:19 +0300

    MainActivity is created. UI is created for both activities. Permissions is written.

--- app/src/main/java/com/example/javamaps/MapsActivity.java
+++ app/src/main/java/com/example/javamaps/MapsActivity.java
@@ -1,13 +1,25 @@
 package com.example.javamaps;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,11 +28,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.javamaps.databinding.ActivityMapsBinding;
+import com.google.android.material.snackbar.Snackbar;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    ActivityResultLauncher<String> permissionLauncher;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    SharedPreferences sharedPreferences;
+    boolean info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +51,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        registerLauncher();
+
+        sharedPreferences = this.getSharedPreferences("com.example.javamaps", MODE_PRIVATE);
+        info = false;
     }
 
     /**
@@ -44,25 +67,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMapLongClickListener(this);
 
         // 48.85959828559606, 2.292925617236018
-        LatLng eiffel = new LatLng(48.85959828559606, 2.292925617236018 );
-        mMap.addMarker(new MarkerOptions().position(eiffel).title("Eiffel Tower"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eiffel, 15f));
+        // LatLng eiffel = new LatLng(48.85959828559606, 2.292925617236018 );
+        // mMap.addMarker(new MarkerOptions().position(eiffel).title("Eiffel Tower"));
+        // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eiffel, 15f));
 
         // casting
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        LocationListener locationListener = new LocationListener() {
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
-                System.out.println("Location: " + location.toString());
+                // System.out.println("Location: " + location.toString());
+
+                info = sharedPreferences.getBoolean("info", false);
+
+                if(!info){
+                    LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+                    sharedPreferences.edit().putBoolean("info", true).apply();
+                }
             }
         };
 
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Snackbar.make(binding.getRoot(), "Permission needed for maps", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // request permission
+                        permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+                    }
+                }).show();
+            } else {
+                // request permission
+                permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
 
+            }
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            mMap.setMyLocationEnabled(true);
+            Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(lastLocation != null){
+                LatLng lastUserLocation = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 15));
+
+            }
+        }
+
+        
        /*
        // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
@@ -70,7 +128,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         */
 
+    }
+
+    private void registerLauncher(){
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onActivityResult(Boolean result) {
+                if(result){
+                    // permission granted
+                    if(ContextCompat.checkSelfPermission(MapsActivity.this,  Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if(lastLocation != null){
+                            LatLng lastUserLocation = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 15));
+
+                        }
+                    }
 
 
+                } else {
+                    Toast.makeText(MapsActivity.this, "Permission Needed!", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        mMap.clear(); // onceki marker lari temizlemek icin
+        mMap.addMarker(new MarkerOptions().position(latLng));
     }
 }
